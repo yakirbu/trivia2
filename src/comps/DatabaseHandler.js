1fff9c9362a7678ec96870b0a5fd46cde9ee4968
Gelman <emil.gelman@intel.com>
2018-03-16 15:17:38 +0200

    added missing dependencies

--- src/comps/DatabaseHandler.js
+++ src/comps/DatabaseHandler.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import * as firebase from "firebase";
 
 // Initialize Firebase  
-var config = {
+const config = {
     apiKey: "AIzaSyAv8yxEEHKdN3WxehmUJY7G6DSWOBYyrO4",
     authDomain: "questions-59ee6.firebaseapp.com",
     databaseURL: "https://questions-59ee6.firebaseio.com",
